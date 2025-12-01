e4c7f37d198163961ac3f90a5cbbad88bd6c933c
marcoguidii <marco.guidi@zucchetti.it>
2025-12-01 15:18:32 +0100

    fix: handle false number list selection when adding

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ while True:
     "2 - Aggiungi un elemento a una lista \n"
     "q - Esci\n"
     "Scegli un'opzione: ").lower().strip()
+
     if choice == "q":
         print("Uscita dal programma.")
         break
@@ -25,20 +26,25 @@ while True:
         for persona in persone:
             print(f"- {persona}")
     elif choice == "2":
-        list_choice = input("A quale lista vuoi aggiungere un elemento? (1 - Animali, 2 - Persone): ").strip()
-        if list_choice == "1":
-            new_animal = input("Inserisci il nome dell'animale da aggiungere: ").strip()
-            if new_animal in animali:
-                print(f"{new_animal} è già presente nella lista degli animali.")
+        while True:
+            list_choice = input("A quale lista vuoi aggiungere un elemento? (1 - Animali, 2 - Persone): ").strip()
+            if list_choice == "1":
+                new_animal = input("Inserisci il nome dell'animale da aggiungere: ").strip()
+                if new_animal in animali:
+                    print(f"{new_animal} è già presente nella lista degli animali.")
+                else:
+                    animali.append(new_animal)
+                    print(f"{new_animal} è stato aggiunto alla lista degli animali.")
+                break
+            elif list_choice == "2":
+                new_person = input("Inserisci il nome della persona da aggiungere: ").strip()
+                if new_person in persone:
+                    print(f"{new_person} è già presente nella lista delle persone.")
+                else:
+                    persone.append(new_person)
+                    print(f"{new_person} è stato aggiunto alla lista delle persone.")
+                break
             else:
-                animali.append(new_animal)
-            print(f"{new_animal} è stato aggiunto alla lista degli animali.")
-        elif list_choice == "2":
-            new_person = input("Inserisci il nome della persona da aggiungere: ").strip()
-            if new_person in persone:
-                print(f"{new_person} è già presente nella lista delle persone.")
-            else:
-                persone.append(new_person)
-                print(f"{new_person} è stato aggiunto alla lista delle persone.")
-        else:
-            print("Scelta non valida. Riprova.")
+                print("Scelta non valida. Riprova.")
+    else:
+        print("Scelta non valida. Riprova.")

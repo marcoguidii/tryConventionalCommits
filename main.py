a920e6c085b6e86fbd635831df40f9c709e6dd54
marcoguidii <marco.guidi@zucchetti.it>
2025-12-01 13:56:00 +0100

    feat: check animal in a list

---
+++ main.py
@@ -0,0 +1,15 @@
+animali = [
+    "cane",
+    "gatto",
+    "elefante",
+    "leone",
+    "tigre",
+    "orso"
+]
+
+choice = input("Inserisci il nome di un animale: ")
+
+if choice in animali:
+    print(f"L'animale '{choice}' è presente nella lista.")
+else:
+    print(f"L'animale '{choice}' non è presente nella lista.")

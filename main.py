0f649e96c2214109080ccaf7b7fa3977d13b0c38
marcoguidii <marco.guidi@zucchetti.it>
2025-12-01 13:59:40 +0100

    feat: add animal not in list

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ if choice in animali:
     print(f"L'animale '{choice}' è presente nella lista.")
 else:
     print(f"L'animale '{choice}' non è presente nella lista.")
+    add_choice = input("Vuoi aggiungerlo alla lista? (sì/no): ").lower().strip()
+    if add_choice == "sì":
+        animali.append(choice)
+        print(f"L'animale '{choice}' è stato aggiunto alla lista.")

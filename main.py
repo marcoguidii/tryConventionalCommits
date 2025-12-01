a9cbd058cc1bb6f264902a25f4d8342c9aecd12d
marcoguidii <marco.guidi@zucchetti.it>
2025-12-01 14:04:36 +0100

    fix: handle case where the animal isn't in the list and you don't want to add it

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ if choice in animali:
     print(f"L'animale '{choice}' è presente nella lista.")
 else:
     print(f"L'animale '{choice}' non è presente nella lista.")
-    add_choice = input("Vuoi aggiungerlo alla lista? (sì/no): ").lower().strip()
-    if add_choice == "sì":
+    add_choice = input("Vuoi aggiungerlo alla lista? (y/n): ").lower().strip()
+    if add_choice == "y":
         animali.append(choice)
         print(f"L'animale '{choice}' è stato aggiunto alla lista.")
+        print("Lista aggiornata:", animali)
+    else:
+        print("Nessuna modifica apportata alla lista. choice", add_choice)

3159864e12eb292354d8df33e3084f99f9ad69ac
marcoguidii <marco.guidi@zucchetti.it>
2025-12-01 14:44:35 +0100

    feat: cyclic request
    you can now excecute the application more time until you press q
    
    questo-footer

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ animali = [
     "orso"
 ]
 
-choice = input("Inserisci il nome di un animale: ").lower().strip()
+while True:
+    choice = input("Inserisci il nome di un animale (q per terminare): ").lower().strip()
+    if choice == "q":
+        print("Uscita dal programma.")
+        break
 
-if choice in animali:
-    print(f"L'animale '{choice}' è presente nella lista.")
-else:
-    print(f"L'animale '{choice}' non è presente nella lista.")
-    add_choice = input("Vuoi aggiungerlo alla lista? (y/n): ").lower().strip()
-    if add_choice == "y":
-        animali.append(choice)
-        print(f"L'animale '{choice}' è stato aggiunto alla lista.")
-        print("Lista aggiornata:", animali)
+    if choice in animali:
+        print(f"L'animale '{choice}' è presente nella lista.")
     else:
-        print("Nessuna modifica apportata alla lista. choice", add_choice)
+        print(f"L'animale '{choice}' non è presente nella lista.")
+        add_choice = input("Vuoi aggiungerlo alla lista? (y/n): ").lower().strip()
+        if add_choice == "y":
+            animali.append(choice)
+            print(f"L'animale '{choice}' è stato aggiunto alla lista.")
+            print("Lista aggiornata:", animali)
+        else:
+            print("Nessuna modifica apportata alla lista.")
